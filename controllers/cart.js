a2af3eddbbe231b59716625ee8e68cad839317ae
Kamzin <olegkamzin@gmail.com>
2023-12-17 13:24:15 +0300

    new

--- controllers/cart.js
+++ controllers/cart.js
@@ -44,7 +44,30 @@ class CartController {
 			return res.json({
 				cart: {
 					deliveryCurrency: "RUR",
-					deliveryOptions: [],
+					deliveryOptions: [{
+						price: 0,
+						type: "DELIVERY",
+						serviceName: "Доставка магазина",
+						dates: {
+							fromDate: formattedDate,
+							toDate: formattedDate,
+							intervals: [
+								{
+									date: formattedDate,
+									fromTime: "09:00",
+									toTime: "21:00"
+								}
+							]
+						},
+						paymentMethods: [
+							"YANDEX",
+							"APPLE_PAY",
+							"GOOGLE_PAY",
+							"TINKOFF_CREDIT",
+							"TINKOFF_INSTALLMENTS",
+							"SBP"
+						]
+					}],
 					items: result,
 					paymentMethods: [
 						"YANDEX",
